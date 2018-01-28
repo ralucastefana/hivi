bb420d978e5b41124899bbf73822d2deeaf7a8d1
Raluca Scortanu <raluca.scortanu@gmail.com>
2018-01-28 22:41:33 +0200

    Am adaugat un comparator pentru functia de sortare
    Functia buildTodaysActivity returneaza lista activitatii in ultimile 24h
    ordonate dupa numarul de accesari ale domeniilor
    
    Mai multe elemente vor fi preluate in viitoare update-uri ale functiei

--- kiwi-extension/js/overview.js
+++ kiwi-extension/js/overview.js
@@ -9,36 +9,68 @@ function getBaseUrl(url) {
     return url;
 }
 
+var uniqueDomains = [];
+var todaysActivity = [];
+
 function buildTodaysActivity() {
-    var inCallBack = 0;
+    var numOfRequests = 0;
 
     var searchOptions = {
         'text': ''
     }
 
-    var domains = [];
-
     chrome.history.search(searchOptions, function(historyItems) {
-        inCallBack = 1;
 
         for(var i = 0, ie = historyItems.length; i < ie; i++) {
             var currentUrl = getBaseUrl(historyItems[i].url);
 
-            if (domains.indexOf(currentUrl) === -1 && currentUrl.startsWith('http')) {
-                domains.push(currentUrl);
+            if (uniqueDomains.indexOf(currentUrl) === -1 && currentUrl.startsWith('http')) {
+                uniqueDomains.push(currentUrl);
             }
-
-            inCallBack = 0;
         }
 
-        if(!inCallBack) {
+        uniqueDomains.forEach(function(item) {
+            chrome.history.getVisits({url: item}, function(visitItems) {
+                var activity = {
+                    'url': item,
+                    'hits': visitItems.length + 1
+                }
+
+                todaysActivity.push(activity);
+
+                numOfRequests--;
+                if(!numOfRequests) {
+                    onAllVisitsProcessed();
+                }
+            });
+            numOfRequests++;
+        });
+
+        if(!numOfRequests) {
             onAllVisitsProcessed();
         }
     });
 
     var onAllVisitsProcessed = function() {
-        for(i = 0, ie = domains.length; i < ie; i++) {
-            console.log(domains[i]);
+
+        function compare(a, b) {
+            const hitsA = a.hits;
+            const hitsB = b.hits;
+
+            let comparison = 0;
+            if(hitsA > hitsB) {
+                comparison = 1;
+            } else if(hitsA < hitsB) {
+                comparison = -1;
+            }
+
+            return comparison * -1; // for reverse order
+        }
+
+        var sortedActivity = todaysActivity.sort(compare);
+
+        for(i = 0, ie = sortedActivity.length; i < ie; i++) {
+            console.log(sortedActivity[i].url + " " + sortedActivity[i].hits);
         }
     };
 }
