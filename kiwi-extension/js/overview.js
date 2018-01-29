48bd7699d17b64f23d6082431929303ee0786c5a
Raluca Scortanu <raluca.scortanu@gmail.com>
2018-01-29 19:37:06 +0200

    Am adaugat tabel paginat pe pagina Overview
    Tabelul ia datele extrase din functiile construite manual anterior

--- kiwi-extension/js/overview.js
+++ kiwi-extension/js/overview.js
@@ -173,5 +173,38 @@ function retrieveActivity(distOption, excludedDomains) {
         for(i = 0, ie = sortedActivity.length; i < ie; i++) {
             console.log(sortedActivity[i].url + " " + sortedActivity[i].hits + " " + sortedActivity[i].mostRecent);
         }
+
+        drawMainTable(sortedActivity);
     };
+}
+
+function drawMainTable(activityArray) {
+    google.charts.load('current', {'packages':['table']});
+    google.charts.setOnLoadCallback(drawTable);
+
+    function drawTable() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'URL');
+        data.addColumn('number', 'Hits');
+        data.addColumn('date', 'Last Visit');
+        data.addRows(activityArray.length);
+
+        for(var i = 0, ie = activityArray.length; i < ie; i++) {
+            data.setCell(i, 0, activityArray[i].url);
+            data.setCell(i, 1, activityArray[i].hits);
+            data.setCell(i, 2, activityArray[i].mostRecent);
+        }
+
+        var table = new google.visualization.Table(document.getElementById('overviewTable'));
+
+        var options = {
+            showRowNumber: true,
+            width: '75%',
+            height: '100%',
+            alternatingRowStyle: true,
+            page: 'enable',
+            pageSize: 10
+        }
+        table.draw(data, options);
+    }
 }
